b9b563f5196f3e68546ad95ea3180441134a8ec1
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-01-15 12:03:56 +0100

    Start of Quiz project

---
+++ Quiz/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      hej hej
+
+      <h1>Quiz</h1>
+
+      
+   
+      </div>
+      
+    </>
+  )
+}
+
+export default App
