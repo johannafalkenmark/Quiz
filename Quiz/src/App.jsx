a1e261bb73f164680f8865694ce0d77c0815f72d
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-01-20 14:37:46 +0100

    started to set up feedback functionality

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import GameMode from "./components/TESTGameMode";
 import Game from "./components/Game";
 import CookieConsent from "react-cookie-consent";
 
--- Quiz/src/components/Game.jsx
+++ Quiz/src/components/Game.jsx
@@ -13,8 +13,15 @@ const Game = () => {
   //State för poängställning
   const [score, setScore] = useState(0);
 
+  //State för feedback efter svar på fråga
+  const [feedback, setFeedback] = useState("");
+  
   //State för timer. Först tidkvar variabel sen metoden som kan ändra den. Börjar på 10(sek).
   const [timeLeft, setTimeLeft] = useState(10);
+
+//feedback effekt:
+
+
 
   //TIMER
 //   useEffect(() => {
@@ -34,6 +41,7 @@ const Game = () => {
 
   
 
+//HÄMTA FRÅGORNA FRÅN FIL VIA FETCH
   const getQuestions = () => {
     fetch("Questions.json")
       .then((response) => {
@@ -50,13 +58,12 @@ const Game = () => {
 
   useEffect(() => {
     getQuestions();
+    // En tom array betyder kör den bara en gång per livscykel:
   }, []);
-  // En tom array betyder kör den bara en gång per livscykel
-
+ 
 
   //Variabel som innehåller json filens array och viss arrayindex. Så att endast en fråga i taget kan visas.
   const currentItem = quizData[questionIndex];
-
   //så att den inte körs innan spelet dragit igång:
   if (!currentItem) {
     return null;
@@ -64,47 +71,76 @@ const Game = () => {
 
   //Stämmer av arrayindex mot längden på arrayen i jsonfilen.
   if (questionIndex >= quizData.length - 1) {
-    //visar poängen:
-    return <div>Your score is {score}</div>;
+    //visar poängen nä spelet är klart/arrayen gåtts igenom:
+    return (
+      <div className="final-text">
+        Thank you for playing!
+        <div className="final-score">
+          Your final score is <p className="final-score-number">{score}</p>
+        </div>
+      </div>
+    );
   }
 
-const checkAnswer = () => {
-if (currentItem.options === currentItem.answer) {
-  setScore(+1);
-  return <div>Rätt svar du får +1 poäng {score} </div>;
+const checkAnswer = (selectedOption) => {
+if (selectedOption === currentItem.answer) {
+  setScore(score +1);
+setFeedback("BRA DET ÄR RÄTT SVAR");
+} else {
+setScore(score - 1);
+setFeedback("DU HADE FEL, Träna lite mer innan du spelar igen");
 }
-if (currentItem.options !== currentItem.answer) {
-   
-      setQuestionIndex(questionIndex + 1);
-      setScore(-5);
-       return <div>Fel svar! {score -1}</div>;
-}
-}
+//Skickar vidare till nästa fråga+timer sätts
+    setTimeLeft(10);
+    setQuestionIndex(questionIndex + 1);
+    //gör feedbacken tom till nästa gång:  HUR GÖR JAG ATT DETTA DRÖJER OCH INTE RADERAS DIREKT       
+    //setFeedback("");
+};
 
   return (
     <>
       <div className="play-text">Lets play!</div>
 
-      
       <div className="question">{currentItem.question}</div>
 
       <ul className="optionslist">
         {currentItem.options.map((option, index) => (
           <li className="option" key={index}>
-            <button className="option-button"
-            onClick={() => {
-        checkAnswer();
-
-            }}> {option} </button>
+            <button
+              className="option-button"
+              onClick={() => {
+                //här blir  option selectedoption:
+                checkAnswer(option);
+              }}
+            >
+              {option}
+            </button>
           </li>
         ))}
       </ul>
 
+      <div className="feedback">
+
+        {/* //HUR GÖR JAG FEEDBACK EFFEKT BÄTTRE. at tdet påverkar vad som
+        skrivs/poppar upp någonstans. */}
+        {feedback}
+      </div>
+
+      <div
+        className={
+          score < 0
+            ? "score-status-low"
+            : score < 2
+            ? "score-status"
+            : "score-status-high"
+        }
+      >
+        Score: {score}
+      </div>
       {/* Om tiden är mindre än fyra sätts klassnamnet hurry annars time */}
       <h3 className={timeLeft < 4 ? "hurry" : "time"}>{timeLeft}</h3>
-      <div className="score-status">SCORE: {score}</div>
-
-      <button className="next-button"
+      <button
+        className="next-button"
         onClick={() => {
           setTimeLeft(10);
           setQuestionIndex(questionIndex + 1);
