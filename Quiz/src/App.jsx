ec78b82a0965df7f99aaeebe5c12c5143b6b5af1
Johanna Falkenmark <johanna.falkenmark@yh.nackademin.se>
2025-01-27 14:36:30 +0100

    fixed positioning etc

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -4,22 +4,18 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Game from "./components/Game";
 import CookieConsent from "react-cookie-consent";
-import Footer from "./components/Footer";
+
 
 function App() {
-
- 
   return (
-    <>
-      <div>
-        <CookieConsent buttonText="OKEY">Accept cookies</CookieConsent>
-
+    <div>
+      <CookieConsent buttonText="OKEY">Accept cookies</CookieConsent>
+      <div className="game-box">
         <h1>Quiz</h1>
-
         <Game />
-        <Footer />
+
       </div>
-    </>
+    </div>
   );
 }
 
